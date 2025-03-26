85375ab304df25c9148791ab23f3ba6717684bf6
DESKTOP-TIO0FNB\Starbruder <starbruder.mc@gmail.com>
2025-03-26 17:00:42 +0100

    MS: Renaming Fix

--- PlayScore/Services/MoonphaseTranslationService.cs
+++ PlayScore/Services/MoonphaseTranslationService.cs
@@ -7,13 +7,13 @@ public sealed class MoonphaseTranslationService : IService
     private readonly Dictionary<string, (int Id, string GermanName)> _moonPhaseData = new()
     {
         { "NEW_MOON",        ( (int)Moonphases.Neumond,              "Neumond") },
-        { "WAXING_CRESCENT", ( (int)Moonphases.ZunehmendeMondsichel, "Zunehmende Sichel") },
+        { "WAXING_CRESCENT", ( (int)Moonphases.ZunehmendeSichel, "Zunehmende Sichel") },
         { "FIRST_QUARTER",   ( (int)Moonphases.ErstesViertel,        "Erstes Viertel") },
         { "WAXING_GIBBOUS",  ( (int)Moonphases.ZunehmenderMond,      "Zunehmender Mond") },
         { "FULL_MOON",       ( (int)Moonphases.Vollmond,             "Vollmond") },
         { "WANING_GIBBOUS",  ( (int)Moonphases.AbnehmenderMond,      "Abnehmender Mond") },
         { "LAST_QUARTER",    ( (int)Moonphases.LetztesViertel,       "Letztes Viertel") },
-        { "WANING_CRESCENT", ( (int)Moonphases.AbnehmendeMondsichel, "Abnehmende Sichel") }
+        { "WANING_CRESCENT", ( (int)Moonphases.AbnehmendeSichel, "Abnehmende Sichel") }
     };
 
     public (int? Id, string? Name) GetMoonPhaseData(string moonPhase)
