6fde312b076d2a64951d9d396919a0245d1c5498
dhi@gecko.de <dhi@gecko.de>
2025-03-26 08:49:57 +0100

    MS: Refactored and improved performance of FormatDayOrMonth() and GetFormattedCurrentDate() in DateTimeService.cs

--- PlayScore/Services/DateTimeService.cs
+++ PlayScore/Services/DateTimeService.cs
@@ -4,19 +4,62 @@ public sealed class DateTimeService : IService
 {
     public static readonly DateOnly CurrentDate = DateOnly.FromDateTime(DateTime.Now);
 
-    private static string GetFormattedDayOrMonth(int dateNumber)
+    private static void FormatDayOrMonth(int dateNumber, Span<char> buffer)
     {
-        var dateNumberString = dateNumber.ToString();
-        return dateNumberString.Length < 2
-            ? "0" + dateNumberString
-            : dateNumberString;
+        // If the number is less than 10, put a leading zero in the buffer.
+        if (dateNumber < 10)
+        {
+            buffer[0] = '0';
+            buffer[1] = (char)('0' + dateNumber);
+            return;
+        }
+
+        buffer[0] = (char)('0' + (dateNumber / 10));
+        buffer[1] = (char)('0' + (dateNumber % 10));
     }
 
     public static string GetFormattedCurrentDate()
     {
-        var day = GetFormattedDayOrMonth(CurrentDate.Day);
-        var month = GetFormattedDayOrMonth(CurrentDate.Month);
+        // Use stack-allocated memory for the result string.
+        Span<char> result = stackalloc char[10];
 
-        return $"{CurrentDate.Year}-{month}-{day}";
+        // Format the year as 4 digits
+        CurrentDate.Year.TryFormat(result[..4], out int _);
+
+        // Insert the dash between year and month
+        result[4] = '-';
+
+        // Format the month into 2 digits
+        Span<char> monthBuffer = stackalloc char[2];
+        FormatDayOrMonth(CurrentDate.Month, monthBuffer);
+        monthBuffer.CopyTo(result.Slice(5, 2));
+
+        // Insert the dash between month and day
+        result[7] = '-';
+
+        // Format the day into 2 digits
+        Span<char> dayBuffer = stackalloc char[2];
+        FormatDayOrMonth(CurrentDate.Day, dayBuffer);
+        dayBuffer.CopyTo(result.Slice(8, 2));
+
+        // Return the formatted result as a string
+        return new string(result);
     }
+
+
+    //private static string GetFormattedDayOrMonth(int dateNumber)
+    //{
+    //    var dateNumberString = dateNumber.ToString();
+    //    return dateNumberString.Length < 2
+    //        ? "0" + dateNumberString
+    //        : dateNumberString;
+    //}
+
+    //public static string GetFormattedCurrentDate()
+    //{
+    //    var day = GetFormattedDayOrMonth(CurrentDate.Day);
+    //    var month = GetFormattedDayOrMonth(CurrentDate.Month);
+
+    //    return $"{CurrentDate.Year}-{month}-{day}";
+    //}
 }
