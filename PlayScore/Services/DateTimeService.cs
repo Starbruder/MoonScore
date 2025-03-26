e71584590a2369e02b8194b1a9e0c3792a7cab37
dhi@gecko.de <dhi@gecko.de>
2025-03-26 08:20:44 +0100

    MS: Added new DateTimeService.cs, in Order to have a Central Point, where we can control the Date and Time for Mock-reasons and other Stuff too

---
+++ PlayScore/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿namespace MoonScore.Services;
+
+public sealed class DateTimeService : IService
+{
+    public static readonly DateOnly CurrentDate = DateOnly.FromDateTime(DateTime.Now);
+
+    public static string GetFormattedCurrentDate()
+        => $"{CurrentDate.Year}-{CurrentDate.Month}-{CurrentDate.Day}";
+}
