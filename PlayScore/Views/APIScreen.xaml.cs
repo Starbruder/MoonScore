32baf9475dc91fca9a767e5daf6c9cf19d531316
DESKTOP-TIO0FNB\Starbruder <starbruder.mc@gmail.com>
2025-03-27 18:46:31 +0100

    MS: Mini Refactoring of cs in new APIScreen

--- PlayScore/Views/APIScreen.xaml.cs
+++ PlayScore/Views/APIScreen.xaml.cs
@@ -1,39 +1,26 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace MoonScore.Views
+namespace MoonScore.Views;
+
+/// <summary>
+/// Interaktionslogik für APIScreen.xaml
+/// </summary>
+public partial class APIScreen : Window
 {
-    /// <summary>
-    /// Interaktionslogik für APIScreen.xaml
-    /// </summary>
-    public partial class APIScreen : Window
+    private readonly IServiceProvider _serviceProvider;
+
+    public APIScreen(IServiceProvider serviceProvider)
     {
-        private readonly IServiceProvider _serviceProvider;
+        InitializeComponent();
 
-        public APIScreen(IServiceProvider serviceProvider)
-        {
-            InitializeComponent();
+        _serviceProvider = serviceProvider;
+    }
 
-            _serviceProvider = serviceProvider;
-        }
-
-        private void OpenMainWindow(object sender, RoutedEventArgs e)
-        {
-            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-            this.Hide();
-        }
+    private void OpenMainWindow(object sender, RoutedEventArgs e)
+    {
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+        this.Hide();
     }
 }
